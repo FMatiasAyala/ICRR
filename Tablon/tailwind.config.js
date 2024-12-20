4a76d6f24070845a04d5e56b9699aba18cee28ef
Matias <mattayala_15@outlook.com>
2024-12-20 11:21:42 -0300

    Actualizacion tailwind.config.js

--- Tablon/tailwind.config.js
+++ Tablon/tailwind.config.js
@@ -1,9 +1,10 @@
-const { nextui } = require("@nextui-org/theme");
+import { nextui } from "@nextui-org/theme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx},",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {},
