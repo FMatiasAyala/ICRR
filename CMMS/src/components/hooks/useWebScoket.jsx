9c10ee60335fcd9ad5e1fc7f78b6ae907ed6ac61
Matias <mattayala_15@outlook.com>
2025-03-10 08:21:34 -0300

    Correccion de multiples conexiones de Websocket.

--- CMMS/src/components/hooks/useWebScoket.jsx
+++ CMMS/src/components/hooks/useWebScoket.jsx
@@ -1,54 +1,51 @@
-// src/hooks/useWebSocket.js
-
 import { useState, useEffect, useCallback } from 'react';
 import { io } from 'socket.io-client'; // Importa socket.io-client
 import { apiWebSocket } from '../../components/utils/Fetch';
 
+let socketInstance = null; // Variable global para mantener una única conexión WebSocket
+
 export const useWebSocket = (initialState = []) => {
   const [estadoEquipos, setEstadoEquipos] = useState(initialState);
-  const [socket, setSocket] = useState(null);
 
-  // Establecer la conexión WebSocket con socket.io
   useEffect(() => {
-    const socketUrl = apiWebSocket; // Asegúrate de que esta URL apunte a tu servidor de WebSocket con socket.io
-    const newSocket = io(socketUrl, {
-      transports: ['websocket'], // Usa solo el transporte WebSocket
-    });
+    if (!socketInstance) {
+      socketInstance = io(apiWebSocket, {
+        transports: ['websocket'], // Usa solo el transporte WebSocket
+      });
 
-    newSocket.on('connect', () => {
-      console.log('Conexión WebSocket establecida');
-    });
+      socketInstance.on('connect', () => {
+        console.log('Conexión WebSocket establecida');
+      });
 
-    newSocket.on('eventoActualizado', (data) => {
-      // Aquí, asumiendo que el servidor emite el evento 'estadoActualizado'
-      const estadoNormalizado = data.estado.toUpperCase();
-      setEstadoEquipos(prevState => ({
-        ...prevState,
-        [data.id_equipo]: estadoNormalizado,
-      }));
-      console.log('Recibido evento de actualización de estado:', data);
-    });
+      socketInstance.on('eventoActualizado', (data) => {
+        const estadoNormalizado = data.estado.toUpperCase();
+        setEstadoEquipos((prevState) => ({
+          ...prevState,
+          [data.id_equipo]: estadoNormalizado,
+        }));
+        console.log('Recibido evento de actualización de estado:', data);
+      });
 
-
-
-    newSocket.on('disconnect', () => {
-      console.log('Conexión WebSocket cerrada');
-    });
-
-    setSocket(newSocket);
+      socketInstance.on('disconnect', () => {
+        console.log('Conexión WebSocket cerrada');
+      });
+    }
 
     // Limpiar la conexión cuando el componente se desmonte
     return () => {
-      newSocket.close();
+      if (socketInstance) {
+        socketInstance.close();
+        socketInstance = null;
+      }
     };
   }, []);
 
   // Función para enviar mensajes a través del WebSocket
   const sendSocketMessage = useCallback((message) => {
-    if (socket) {
-      socket.emit('enviarMensaje', message); // Usamos emit en lugar de send
+    if (socketInstance) {
+      socketInstance.emit('enviarMensaje', message);
     }
-  }, [socket]);
+  }, []);
 
   return {
     estadoEquipos,
@@ -56,6 +53,3 @@ export const useWebSocket = (initialState = []) => {
     sendSocketMessage,
   };
 };
-
-
-
